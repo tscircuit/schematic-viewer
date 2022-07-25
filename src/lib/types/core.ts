37fa96bec33b3d057b1e282a6d7ef0a2f472d0b2
seveibar <seveibar@gmail.com>
2022-07-24 21:29:07 -0700

    more types

---
+++ src/lib/types/core.ts
@@ -0,0 +1,127 @@
+export interface SchematicConfig {
+  type: "schematic_config"
+}
+
+export interface Bounds {
+  left: number
+  top: number
+  right: number
+  bottom: number
+}
+
+export interface Position {
+  left: number
+  top: number
+}
+
+export interface SourceConfig {
+  type: "source_config"
+}
+
+export interface SchematicGroup {
+  type: "schematic_group"
+  source_group_id: string
+  bounds: Bounds
+  children_schematic_component_ids: string[]
+  children_schematic_trace_ids: string[]
+}
+
+export interface SchematicComponent {
+  type: "schematic_component"
+  bounds: Bounds
+  source_component_id: string
+}
+
+export interface SchematicTrace {
+  type: "schematic_trace"
+  source_trace_id: string
+  route: Array<{
+    position: Position
+    start_schematic_port_id: string
+    end_schematic_port_id: string
+  }>
+}
+
+export interface SchematicPort {
+  schematic_port_id: string
+  source_port_id: string
+}
+
+export interface SchematicPort {
+  type: "schematic_port"
+  position: Position
+}
+
+export interface PCBTrace {
+  type: "pcb_trace"
+  bounds: Bounds
+  source_trace_id: string
+  pcb_trace_id: string
+}
+
+export interface PCBComponent {
+  type: "pcb_component"
+  pcb_component_id: string
+  source_component_id: string
+}
+
+export interface PCBGroup {
+  type: "pcb_group"
+  source_group_id: string
+}
+
+export interface PCBConfig {
+  type: "pcb_config"
+  dimension_unit: "mm"
+}
+
+export interface SourceTrace {
+  type: "source_trace"
+  source_trace_id: string
+  source_component_: string
+  source_component_id_2: string
+}
+
+export interface SourceGroup {
+  type: "source_group"
+  source_group_id: string
+  name: string
+  children_source_component_ids: string
+}
+
+export interface SourceComponent {
+  type: "source_component"
+  source_component_id: string
+  name: string
+}
+
+export interface Project {
+  type: "project"
+  schematic_config: SchematicConfig
+  schematic_components: SchematicComponent[]
+  schematic_groups: SchematicGroup[]
+  schematic_traces: SchematicTrace[]
+  pcb_config: PCBConfig
+  pcb_groups: PCBGroup[]
+  pcb_components: PCBComponent[]
+  pcb_traces: PCBTrace[]
+  source_config: SourceConfig
+  source_traces: SourceTrace[]
+  source_groups: SourceGroup[]
+  source_components: SourceComponent[]
+}
+
+export type AnyObject =
+  | Project
+  | SourceConfig
+  | SourceComponent
+  | SourceGroup
+  | SourceTrace
+  | PCBTrace
+  | PCBComponent
+  | PCBGroup
+  | PCBConfig
+  | SchematicGroup
+  | SchematicComponent
+  | SchematicTrace
+  | SchematicConfig
