aaa00091f6be452ee4ea05a1da6e6ded6aba8625
seveibar <seveibar@gmail.com>
2022-07-31 12:30:48 -0700

    introduce ports builder

--- src/lib/project/builder/component-builder.ts
+++ src/lib/project/builder/component-builder.ts
@@ -1,11 +1,15 @@
+import { SchematicComponent } from "./../../types/core"
 import * as Type from "lib/types"
 import { Except, Simplify } from "type-fest"
 import { ProjectBuilder } from "./project-builder"
+import { PortsBuilder, createPortsBuilder } from "./ports-builder"
 
 export type ComponentBuilderCallback = (cb: ComponentBuilder) => unknown
 export interface ComponentBuilder {
   project_builder: ProjectBuilder
+  ports: PortsBuilder
   setName: (name: string) => ComponentBuilder
+  tag: (tag: string) => ComponentBuilder
   setSourceProperties<T extends Type.SourceComponentFType>(
     ftype: T,
     properties: Simplify<
@@ -23,14 +27,23 @@ export interface ComponentBuilder {
 export const createComponentBuilder = (
   project_builder: ProjectBuilder
 ): ComponentBuilder => {
-  const builder: ComponentBuilder = { project_builder } as any
-  const internal: any = {}
+  const builder: ComponentBuilder = {
+    project_builder,
+    ports: createPortsBuilder(project_builder),
+  } as any
+  const internal: any = {
+    tags: [],
+  }
 
-  builder.setName = (name: string) => {
+  builder.tag = (tag) => {
+    internal.tags.push(tag)
+    return builder
+  }
+  builder.setName = (name) => {
     internal.name = name
     return builder
   }
-  builder.setSourceProperties = (ftype: string, props: any) => {
+  builder.setSourceProperties = (ftype, props) => {
     internal.source_properties = {
       ftype,
       ...props,
@@ -55,16 +68,22 @@ export const createComponentBuilder = (
     const elements: Type.AnyElement[] = []
     const { ftype } = internal.source_properties
     const source_component_id = project_builder.getId(ftype)
-    elements.push({
+    const schematic_component_id = project_builder.getId(
+      `schematic_component_${ftype}`
+    )
+    const pcb_component_id = project_builder.getId(`pcb_component_${ftype}`)
+    const source_component = {
       type: "source_component",
       source_component_id,
       name: internal.name,
       ...internal.source_properties,
-    })
-    elements.push({
+    }
+    elements.push(source_component)
+
+    const schematic_component: SchematicComponent = {
       type: "schematic_component",
       source_component_id,
-      schematic_component_id: project_builder.getId(`sch_${ftype}`),
+      schematic_component_id,
       rotation: internal.schematic_rotation ?? 0,
       size:
         ftype === "simple_capacitor"
@@ -76,11 +95,29 @@ export const createComponentBuilder = (
             }
           : { width: 1, height: 1 },
       center: internal.schematic_position || { x: 0, y: 0 },
-    })
+    }
+    elements.push(schematic_component)
+
+    builder.ports.setSchematicComponent(schematic_component_id)
+    builder.ports.setSourceComponent(source_component_id)
+
+    // Ports can usually be determined via the ftype and dimensions
+    switch (source_component.ftype) {
+      case "simple_capacitor": {
+        builder.ports.add("left", { x: 0, y: 0 })
+        builder.ports.add("right", { x: 1, y: 0 })
+      }
+      case "simple_resistor": {
+        builder.ports.add("left", { x: 0, y: 0 })
+        builder.ports.add("right", { x: 1, y: 0 })
+      }
+    }
+    elements.push(...builder.ports.build())
+
     elements.push({
       type: "pcb_component",
       source_component_id,
-      pcb_component_id: project_builder.getId(`pcb_${ftype}`),
+      pcb_component_id,
     })
     return elements
   }
---
+++ src/lib/project/builder/ports-builder.ts
@@ -0,0 +1,73 @@
+import * as Type from "lib/types"
+import { Except, Simplify } from "type-fest"
+import { ProjectBuilder } from "./project-builder"
+
+export type PortBuilderCallback = (cb: PortsBuilder) => unknown
+export interface PortsBuilder {
+  project_builder: ProjectBuilder
+  add: (portName: string, schematicPosition: Type.Point) => PortsBuilder
+  setSourceComponent: (source_component_id: string) => PortsBuilder
+  setSchematicComponent: (schematic_component_id: string) => PortsBuilder
+  setPCBComponent: (pcb_component_id: string) => PortsBuilder
+  build(): Type.AnyElement[]
+}
+
+export const createPortsBuilder = (
+  project_builder: ProjectBuilder
+): PortsBuilder => {
+  const builder: PortsBuilder = { project_builder } as any
+
+  const internal = {
+    source_component_id: "",
+    schematic_component_id: "",
+    pcb_component_id: "",
+    ports: [] as Array<{
+      name: string
+      schematic_position: Type.Point
+    }>,
+  }
+
+  builder.add = (name, schematic_position) => {
+    internal.ports.push({
+      name,
+      schematic_position,
+    })
+    return builder
+  }
+
+  builder.setSourceComponent = (source_component_id) => {
+    internal.source_component_id = source_component_id
+    return builder
+  }
+  builder.setSchematicComponent = (schematic_component_id) => {
+    internal.schematic_component_id = schematic_component_id
+    return builder
+  }
+  builder.setPCBComponent = (pcb_component_id) => {
+    internal.pcb_component_id = pcb_component_id
+    return builder
+  }
+
+  builder.build = () => {
+    return internal.ports.flatMap((port) => {
+      const source_port_id = project_builder.getId("source_port")
+      const schematic_port_id = project_builder.getId("schematic_port")
+      const pcb_port_id = project_builder.getId("pcb_port")
+      return [
+        {
+          type: "source_port",
+          source_port_id,
+          source_component_id: internal.source_component_id,
+        },
+        {
+          type: "schematic_port",
+          schematic_port_id,
+          source_port_id,
+          position: port.schematic_position,
+        },
+      ] as Type.AnyElement[]
+    }) as Type.AnyElement[]
+  }
+
+  return builder
+}
