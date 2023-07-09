1a4dc9d0b4f5074dfef16f49bdef1c7de1250f3b
Severin Ibarluzea <seveibar@gmail.com>
2023-07-09 00:10:18 -0700

    fix: update release.config.js

--- release.config.js
+++ release.config.js
@@ -3,8 +3,13 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
-    "@semantic-release/npm",
-    "@semantic-release/github",
+    ["@semantic-release/npm", { npmPublish: true }],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json"],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ]
   ],
 }
