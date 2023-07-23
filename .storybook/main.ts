4400be06980ca2e8dc91e696899a058930934b53
Severin Ibarluzea <seveibar@gmail.com>
2023-07-22 20:29:55 -0700

    better storybook, add deeper

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,11 @@
 import type { StorybookConfig } from "@storybook/nextjs"
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
+  addons: [],
   framework: {
     name: "@storybook/nextjs",
     options: {},
   },
-  docs: {
-    autodocs: "tag",
-  },
+  docs: {},
 }
 export default config
