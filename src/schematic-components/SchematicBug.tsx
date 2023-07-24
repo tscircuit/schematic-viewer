1c8c8115fd6b4c8016c68f5edd30e582fe3bd59e
Severin Ibarluzea <seveibar@gmail.com>
2023-07-23 23:52:03 -0700

    fix: three sided bug renders properly

--- src/schematic-components/SchematicBug.tsx
+++ src/schematic-components/SchematicBug.tsx
@@ -1,6 +1,8 @@
 import * as Type from "lib/types"
 import SVGPathComponent from "./SVGPathComponent"
 import range from "lodash/range"
+import { getPortPosition, getPortArrangementSize } from "@tscircuit/builder"
+import getSVGPathBounds from "lib/utils/get-svg-path-bounds"
 
 interface Props {
   component: {
@@ -10,41 +12,61 @@ interface Props {
 }
 
 export const SchematicBug = ({ component: { source, schematic } }: Props) => {
-  const ports_arrangement = schematic.port_arrangement!
-  const port_labels = schematic.port_labels!
-  const bugw = 30 // bug width
-  const rh = 15 // row height
-  const pd = 7.5 // port distance, the line for each port
-  const bugh =
-    Math.max(ports_arrangement.left_size, ports_arrangement.right_size) * rh
+  const ports_arrangement = {
+    top_size: 0,
+    bottom_size: 0,
+    ...schematic.port_arrangement,
+  }
+  // const port_labels = schematic.port_labels!
+  const bugw = schematic.size.width // bug width
+  const bugh = schematic.size.height
+  const { total_ports } = getPortArrangementSize(ports_arrangement)
   // TODO throw if schematic.size doesn't match computed ports_arrangement size
+
+  const paths = [
+    {
+      stroke: "red",
+      strokeWidth: 0.02,
+      d: `M ${-bugw / 2} ${-bugh / 2} L ${bugw / 2} ${-bugh / 2} L ${
+        bugw / 2
+      } ${bugh / 2} L ${-bugw / 2} ${bugh / 2}Z`,
+    },
+    ...range(1, total_ports + 1).map((portNum) => {
+      const pos = getPortPosition(ports_arrangement, portNum)
+
+      const x2 =
+        pos.side === "left"
+          ? -bugw / 2
+          : pos.side === "right"
+          ? bugw / 2
+          : pos.x
+      const y2 =
+        pos.side === "top"
+          ? -bugh / 2
+          : pos.side === "bottom"
+          ? bugh / 2
+          : pos.y
+
+      return {
+        stroke: "red",
+        strokeWidth: 0.02,
+        d: `M ${pos.x} ${pos.y} L ${x2} ${y2}`,
+      }
+    }),
+  ]
+
+  const actualSize = getSVGPathBounds(paths.map((p) => p.d).join(" "))
+  const actualCenter = {
+    x: (actualSize.minX + actualSize.maxX) / 2,
+    y: (actualSize.minY + actualSize.maxY) / 2,
+  }
+
   return (
     <SVGPathComponent
       rotation={schematic.rotation}
-      center={schematic.center}
-      size={schematic.size}
-      paths={[
-        {
-          stroke: "red",
-          strokeWidth: 1,
-          d: `M 0 0 L ${bugw} 0 L ${bugw} ${bugh} L 0 ${bugh}Z`,
-        },
-        ...range(
-          0,
-          ports_arrangement.left_size + ports_arrangement.right_size
-        ).map((i) => {
-          const ls = ports_arrangement.left_size
-          const left = i < ls
-          const rowi = i % ls
-          const p1 = [left ? 0 : bugw, rh / 2 + rowi * rh]
-          const rd = [left ? -pd : pd, 0]
-          return {
-            stroke: "red",
-            strokeWidth: 1,
-            d: `M ${p1.join(" ")} l ${rd.join(" ")}`,
-          }
-        }),
-      ]}
+      center={actualCenter}
+      size={actualSize}
+      paths={paths}
     />
   )
 }
