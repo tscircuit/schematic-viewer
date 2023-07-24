c40342b07296b37b6117bfa05f963544053fe3b9
Severin Ibarluzea <seveibar@gmail.com>
2023-07-23 20:11:44 -0700

    demonstrate three bug issue

--- src/stories/three-sided-bug.stories.tsx
+++ src/stories/three-sided-bug.stories.tsx
@@ -1,5 +1,25 @@
+import { Schematic } from "Schematic"
+
 export const ThreeSidedBug = () => {
-  return "hi"
+  return (
+    <Schematic style={{ height: 600 }}>
+      <bug
+        name="B1"
+        port_arrangement={{
+          left_size: 3,
+          right_size: 3,
+          top_size: 0,
+          bottom_size: 5,
+        }}
+        sch_cx={8}
+        sch_cy={3}
+        port_labels={{
+          "1": "D0",
+          "2": "D1",
+        }}
+      />
+    </Schematic>
+  )
 }
 
 export default {
