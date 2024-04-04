6345cb79b44b052066123bce4aaa45667a37186e
seveibar <seveibar@gmail.com>
2024-04-03 22:27:33 -0700

    fix: remove some schematic options

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from "tsup"
 export default defineConfig({
   tsconfig: "./tsconfig.json",
   entry: ["./src/index.ts"],
-  format: ["cjs"],
-  treeshake: true,
   dts: true,
   sourcemap: true,
-  clean: true,
 })
