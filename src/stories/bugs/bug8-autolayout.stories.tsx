714e6a0189cd7ba887ffb20a4add964ba086417e
seveibar <seveibar@gmail.com>
2024-05-26 22:31:18 -0700

    add story with example of schematic autolayout

--- src/stories/bugs/bug8-autolayout.stories.tsx
+++ src/stories/bugs/bug8-autolayout.stories.tsx
@@ -1,20 +1,45 @@
 import { Schematic } from "../../Schematic"
-import * as Layout from "@tscircuit/layout"
-
-console.log({ Layout })
+import { layout } from "@tscircuit/layout"
+import { useBug, useResistor, useCapacitor } from "@tscircuit/react-fiber"
 
 export const Bug8Autolayout = () => {
+  const U1 = useBug("U1", {
+    pinLabels: {
+      1: "VCC",
+      2: "D0",
+      3: "D1",
+      4: "GND",
+      5: "INP",
+      6: "THR",
+    },
+    schPortArrangement: {
+      leftSize: 3,
+      topSize: 0,
+      bottomSize: 0,
+      rightSize: 3,
+    },
+  })
+
+  const R1 = useResistor("R1", { resistance: "10k" })
+  const R2 = useResistor("R2", { resistance: "1k" })
+  const R3 = useResistor("R3", { resistance: "5k" })
+  const C1 = useCapacitor("C1", { capacitance: "1uF" })
+
   return (
     <div>
       <Schematic style={{ height: 500 }}>
         <board
-          // layout={layout().autoLayoutSchematic()}
+          layout={layout().autoLayoutSchematic()}
+          pcbCenterX={0}
+          pcbCenterY={0}
           width={"10mm"}
           height={"10mm"}
         >
-          <resistor name="R1" resistance="10 ohm" schX={2} schY={1} />
-          <resistor resistance="1k" schX={1} schY={2} name="main_power" />
-          <trace path={[".main_power > port.right", ".R1 > port.left"]} />
+          <U1 />
+          <R1 left={U1.D1} right={U1.VCC} />
+          <R2 left={U1.D0} right={U1.GND} />
+          <R3 left={U1.INP} right={U1.THR} />
+          <C1 left={U1.INP} right={U1.THR} />
         </board>
       </Schematic>
     </div>
