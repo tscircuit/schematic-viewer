63d9e776000d068b2ffd14c3e8c50969b2d49cee
seveibar <seveibar@gmail.com>
2024-12-09 21:28:01 -0800

    initial vite and cosmos config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import reactCosmos from "react-cosmos-plugin-vite"
 import { resolve } from "path"
 
 export default defineConfig({
-  plugins: [react(), reactCosmos],
-  root: "vite",
+  plugins: [react()],
   resolve: {
     alias: {
       lib: resolve(__dirname, "./lib")
     }
-  }
+  },
 })
