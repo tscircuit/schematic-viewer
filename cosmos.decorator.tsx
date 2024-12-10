3bbbbdf3ee0358ec1f06359bf3fb6b60336117b3
seveibar <seveibar@gmail.com>
2024-12-09 22:46:31 -0800

    add decorator to simplify examples

---
+++ cosmos.decorator.tsx
@@ -0,0 +1,3 @@
+export default ({ children }: { children: React.ReactNode }) => (
+  <div style={{ width: "100vw", height: "100vh" }}>{children}</div>
+)
--- examples/resistor-and-capacitor.fixture.tsx
+++ examples/resistor-and-capacitor.fixture.tsx
@@ -2,15 +2,13 @@ import { SchematicViewer } from "lib/components/SchematicViewer"
 import { renderToCircuitJson } from "lib/dev/render-to-circuit-json"
 
 export default () => (
-  <div style={{ width: "100vw", height: "90vh" }}>
-    <SchematicViewer
-      circuitJson={renderToCircuitJson(
-        <board width="10mm" height="10mm">
-          <resistor name="R1" resistance={1000} schX={-2} />
-          <capacitor name="C1" capacitance="1uF" schX={2} />
-          <trace from=".R1 .pin2" to=".C1 .pin1" />
-        </board>,
-      )}
-    />
-  </div>
+  <SchematicViewer
+    circuitJson={renderToCircuitJson(
+      <board width="10mm" height="10mm">
+        <resistor name="R1" resistance={1000} schX={-2} />
+        <capacitor name="C1" capacitance="1uF" schX={2} />
+        <trace from=".R1 .pin2" to=".C1 .pin1" />
+      </board>,
+    )}
+  />
 )
