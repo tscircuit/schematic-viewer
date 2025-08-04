8aff4740872edad99781fa7218e2dcaaf879870f
ShiboSoftwareDev <shibosoftwaredev@gmail.com>
2025-08-04 19:17:09 +0200

    use the dep tscircuit

--- examples/example9-spice-simulation.fixture.tsx
+++ examples/example9-spice-simulation.fixture.tsx
@@ -1,17 +1,41 @@
 import { renderToCircuitJson } from "lib/dev/render-to-circuit-json"
 import { SchematicViewer } from "lib/index"
 import type { CircuitJson } from "circuit-json"
+import { sel } from "tscircuit"
 
 const circuit = (
-  <board width="10mm" height="10mm">
-    <resistor name="R1" resistance={1000} schX={-2} />
-    <capacitor name="C1" capacitance="1uF" schX={2} />
-    {/* <trace from=".R1 .pin2" to=".C1 .pin1" />
-    <powersource voltage="5V" name="V1" schX={0} schY={-3} />
-    <trace from=".V1 .positive" to=".R1 .pin1" />
-    <trace from=".V1 .negative" to=".C1 .pin2" />
-    <ground name="GND" schX={0} schY={-5} />
-    <trace from=".V1 .negative" to=".GND .gnd" /> */}
+  <board width={16} height={16}>
+    <chip
+      name="V1"
+      footprint="sot23"
+      pinLabels={{
+        pin1: "VOUT",
+        pin2: "GND",
+      }}
+      pinAttributes={{
+        VOUT: { providesPower: true, providesVoltage: 5 },
+        GND: { providesGround: true },
+      }}
+    />
+
+    <resistor name="R1" resistance="1k" footprint="0402" pcbX={4} pcbY={4} />
+    <resistor name="R2" resistance="2k" footprint="0402" pcbX={-4} pcbY={-4} />
+    <capacitor
+      name="C1"
+      capacitance="10uF"
+      footprint="0402"
+      pcbX={0}
+      pcbY={-2}
+    />
+
+    <trace from={"net.VOUT"} to={sel.R1.pin1} />
+    <trace from={".V1 > .VOUT"} to={"net.VOUT"} />
+    <trace from={sel.R1.pin2} to={sel.R2.pin1} />
+    <trace from={sel.R2.pin2} to={"net.GND"} />
+    <trace from={"net.GND"} to={".V1 > .GND"} />
+
+    <trace from={sel.C1.pin1} to={sel.R1.pin2} />
+    <trace from={sel.C1.pin2} to={"net.GND"} />
   </board>
 )
 
