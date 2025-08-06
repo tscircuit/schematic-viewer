a1478490ce85465f2f35f22fc90931d7722cd7ad
ShiboSoftwareDev <shibosoftwaredev@gmail.com>
2025-08-06 15:02:50 +0200

    ts ignore

--- lib/hooks/useSpiceSimulation.ts
+++ lib/hooks/useSpiceSimulation.ts
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import type * as EecircuitEngine from "../types/eecircuit-engine"
+// @ts-ignore
 import { getSpiceSimulationWorkerBlobUrl } from "../workers/spice-simulation.worker.blob.js"
 
 // Types from eecircuit-engine interface
