9d168fe27a2f879ec00e4f01af795092a82e50ff
Mustafa Mulla <mustafamulla765@gmail.com>
2026-03-05 17:12:59 +0530

    chore: update tscircuit dep

---
+++ examples/example19-custom-symbol.fixture.tsx
@@ -0,0 +1,36 @@
+const triangleSvgPath = "M -0.5 -0.7 L -0.5 0.7 L 0.7 0 Z"
+// Rectangle (gate style)
+const rectSvgPath = "M -0.6 -0.6 L 0.6 -0.6 L 0.6 0.6 L -0.6 0.6 Z"
+
+export default () => (
+  <board width="20mm" height="20mm" routingDisabled>
+      <chip
+        name="U1"
+        symbol={
+          <symbol>
+            <schematicpath
+              svgPath={triangleSvgPath}
+              strokeWidth={0.05}
+              isFilled={false}
+            />
+            <port name="IN+" direction="left" />
+            <port name="IN-" direction="left" />
+            <port name="OUT" direction="right" />
+          </symbol>
+        }
+      />
+      <chip
+        name="U2"
+        symbol={
+          <symbol>
+            <schematicpath
+              svgPath={rectSvgPath}
+              strokeWidth={0.05}
+              isFilled={false}
+            />
+            <port name="Y" direction="right" />
+          </symbol>
+        }
+      />
+    </board>
+)
