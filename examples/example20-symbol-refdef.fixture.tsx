f8c644d114f2bf98cf39a9926298dc3a9c29fc2f
Manish chaudhary <125847751+techmannih@users.noreply.github.com>
2026-03-18 07:36:20 +0530

    feat: add symbol-refdef example with custom op-amp symbol and update tscircuit dependency (#170)
    * feat: add symbol-refdef example with custom op-amp symbol and update tscircuit dependency
    
    * put fixture in usememo

---
+++ examples/example20-symbol-refdef.fixture.tsx
@@ -0,0 +1,90 @@
+import { useMemo } from "react"
+import { SchematicViewer } from "lib/components/SchematicViewer"
+import { renderToCircuitJson } from "lib/dev/render-to-circuit-json"
+
+export default () => {
+  const circuitJson = useMemo(
+    () =>
+      renderToCircuitJson(
+        <board width="10mm" height="10mm">
+          <chip
+            name="U1"
+            symbol={
+              <symbol>
+                <schematictext schX={0} schY={1} text="{NAME}" fontSize={0.2} />
+                <schematicline
+                  x1={-0.5}
+                  y1={-0.7}
+                  x2={-0.5}
+                  y2={0.7}
+                  strokeWidth={0.05}
+                />
+
+                <schematicline
+                  x1={-0.5}
+                  y1={0.7}
+                  x2={0.7}
+                  y2={0}
+                  strokeWidth={0.05}
+                />
+
+                <schematicline
+                  x1={0.7}
+                  y1={0}
+                  x2={-0.5}
+                  y2={-0.7}
+                  strokeWidth={0.05}
+                />
+
+                <schematictext
+                  schX={-0.35}
+                  schY={0.35}
+                  text="+"
+                  fontSize={0.3}
+                />
+
+                <schematictext
+                  schX={-0.35}
+                  schY={-0.35}
+                  text="-"
+                  fontSize={0.3}
+                />
+
+                <port
+                  name="IN_POS"
+                  schX={-1}
+                  schY={0.35}
+                  direction="left"
+                  schStemLength={0.5}
+                />
+
+                <port
+                  name="IN_NEG"
+                  schX={-1}
+                  schY={-0.35}
+                  direction="left"
+                  schStemLength={0.5}
+                />
+
+                <port
+                  name="OUT"
+                  schX={1.2}
+                  schY={0}
+                  direction="right"
+                  schStemLength={0.5}
+                />
+              </symbol>
+            }
+          />
+        </board>,
+      ),
+    [],
+  )
+
+  return (
+    <SchematicViewer
+      circuitJson={circuitJson}
+      containerStyle={{ height: "100%" }}
+    />
+  )
+}
