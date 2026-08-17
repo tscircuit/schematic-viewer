a2e6c4dff61969dda75afbe2dc9798977393111c
seveibar <seveibar@gmail.com>
2026-08-17 13:15:36 -0700

    Use released PCB bounds utilities

--- lib/utils/component-details.ts
+++ lib/utils/component-details.ts
@@ -2,7 +2,7 @@ import {
   type PcbBounds,
   getPcbElementBounds,
   getPcbElementsWithinBounds,
-} from "circuit-json-util-preview/lib/get-bounds-of-pcb-elements"
+} from "@tscircuit/circuit-json-util"
 import type {
   CadComponent,
   CircuitJson,
@@ -11,7 +11,7 @@ import type {
 } from "circuit-json"
 import { gzipSync, strToU8 } from "fflate"
 
-export type { PcbBounds } from "circuit-json-util-preview/lib/get-bounds-of-pcb-elements"
+export type { PcbBounds } from "@tscircuit/circuit-json-util"
 
 export type SourceComponent = Extract<
   CircuitJson[number],
